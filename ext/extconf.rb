413539da5986c0b1784cddfe868add6f7d15a76f
Dmitry Dzema <dmitry.dzema@fyber.com>
2015-02-12 10:18:42 +0100

    Do not check for method which was removed

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -7,4 +7,3 @@ if have_library('stdc++') and have_library('boost_thread-mt') and have_library('
 end
 
 have_func('rb_thread_call_without_gvl', 'ruby/thread.h')
-have_func('rb_thread_blocking_region', 'ruby.h')
